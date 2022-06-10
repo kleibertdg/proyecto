b3ee71e3dc0408d30735f51683a58ca4b8a22f82
kleibertdg <kdominguez.1037@unimar.edu.ve>
2022-06-10 11:46:55 -0400

    Se sube por primera vez

---
+++ views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Inicio - MaaS</title>
+        <link rel="stylesheet" href= "file:///C:/laragon/www/proyecto/resources/css/app.css" />
+
+    </head>
+    <body>
+        <header>
+            <a href="">
+                <h2 class="name-company">InformaticsGuards SpA</h2>
+            </a>
+            <nav>
+                <a href="" class="nav.link">Inicia sesión</a>
+                <a href="" class="nav.link">Regístrate</a>
+            </nav>
+        </header>
+
+        <section class="container">
+            <h2 class="label2">Inicia sesión</h2>
+            <form class="id_form">
+                <div class="font">ID n°:</div>
+                <input type = "text" name="">
+                <div class="font font2">Contraseña:</div>
+                <input type = "text2" name="">
+                <button type="submit">Log In</button>
+            </form>
+        </section>
+        <footer>
+            <div class="footer-content">
+                <h4>Síguenos</h4>
+                <p>Propiedad de: <br> InformaticsGuards SpA</p>
+            </div>
+        </footer>
+
+    </body>
+
+</html>
